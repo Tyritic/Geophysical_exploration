8b2b44ea2b1ddf7f2e88d87573aefcaa1196d5cd
Tyritic <1553206659@qq.com>
2024-10-16 12:21:53 +0800

    完成JWT配置类的编写以及项目配置文件

--- src/main/java/com/example/project/common/properties/jwtProperties.java
+++ src/main/java/com/example/project/common/properties/jwtProperties.java
@@ -1,9 +1,15 @@
 package com.example.project.common.properties;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component //控制反转，将该类交给spring容器管理
+@Data //自动生成getter和setter方法
+@ConfigurationProperties(prefix = "project.jwt") //自动装配，读取配置文件中以jwt开头的配置
+
 public class jwtProperties {
+
     /**
      * 微信小程序用户生成jwt令牌相关配置
      */
